c315e176981670a6596fb5df445afe24e4cd0ac3
MohamedFares09 <mofares010@gmail.com>
2025-05-28 21:07:38 +0300

    project filled

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:s7tk/screens/home_screens.dart';
-import 'package:s7tk/screens/login_screens.dart.dart';
-import 'package:s7tk/screens/signup_screens.dart';
-import 'package:s7tk/screens/splash_screens.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -15,12 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
-        LoginScreens.id:(context)=>LoginScreens(),
-        SignupScreens.id : (context)=>SignupScreens(),
-       HomeScreens.id : (context)=>HomeScreens(),
-      },
-    home: SplashScreens(),
+      
+     home: Scaffold(),
     );
   }
 }
